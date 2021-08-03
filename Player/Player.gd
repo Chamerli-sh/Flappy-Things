c33cbc11499f7a184bbaff44a51b17b1d5fb125d
Fares Setbel <53369406+Roombabatata@users.noreply.github.com>
2021-08-04 00:11:17 +0100

    Adding the player and his movement script

---
+++ Player/Player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const MAX_FALL_SPEED = 200
+const JUMP_SPEED = 200
+const GRAVITY = 10
+
+
+var motion : Vector2
+
+func _physics_process(delta):
+	if Input.is_action_just_released("jump"):
+		motion.y = -JUMP_SPEED
+	
+	motion.y += GRAVITY
+	if motion.y > MAX_FALL_SPEED:
+		motion.y = MAX_FALL_SPEED
+	
+	motion = move_and_slide(motion, UP)
