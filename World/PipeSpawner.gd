24d2a82bc759a02436c784a696a7d087e050b042
Roombabatata <fares.setbels@gmail.com>
2021-08-04 09:09:05 +0100

    Give Random Position When Spawning

--- World/PipeSpawner.gd
+++ World/PipeSpawner.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 onready var pipe_node = preload("res://Pipe/Pipe.tscn")
 
-func spawn():
+func spawn(y):
 	var pipe = pipe_node.instance()
 	add_child(pipe)
+	pipe.global_position.y = y
 
 func _on_Timer_timeout():
-	spawn()
-	print("Hello World!")
+	var y = rand_range(-64, 64)
+	spawn(y)
